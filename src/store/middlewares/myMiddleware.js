b63c805fa961e2102f745ecd57a12571d319fdad
BeTweak <ys@betweak.com>
2024-02-21 11:53:13 +0900

    with redux-thunk

--- src/store/middlewares/myMiddleware.js
+++ src/store/middlewares/myMiddleware.js
@@ -1,7 +1,37 @@
 const myMiddleware = (store) => (next) => (action) => {
-  // 리듀서에 전달되기전 로직
+  /**
+   * next: dispatch (다음 미들웨어 혹은 다음 리듀서 실행)
+   * action: 컴포넌트로부터 전달받은 action
+   * store:
+   *    - getState() # 현재 state객체 접근하는 함수
+   *    - dispatch() # dispatch 하는 함수
+   */
+
   let result = next(action);
+  if (result === "Success") {
+    store.dispatch({
+      type: "todo/success",
+      payload: {},
+    });
+  } else {
+    store.dispatch({
+      type: "todo/fail",
+      payload: {},
+    });
+  }
+
   // 리듀서에서 변경된 state를 활용한 로직
+};
+const mymid2 = function (store) {
+  return function (next) {
+    return function (action) {
+      console.log(store);
+      console.log(next);
+      console.log(action);
+
+      next(action);
+    };
+  };
 };
 
 export function myMid(store) {
